6c514a6d0a86a5e4900ff5fe4d36178388b0a353
profmiranda <profpedromiranda87@gmail.com>
2023-06-08 11:11:15 -0400

    Update great-solution-2023.py

--- two-sum/great-solution-2023.py
+++ two-sum/great-solution-2023.py
@@ -6,5 +6,13 @@ class Solution:
         d = {}
         for i, j in enumerate(nums):
             r = target - j
+            print(f" current index: {i}, current num: {j}, r = {r}")
             if r in d: return [d[r], i]
             d[j] = i
+            print(f"current d[j] = i: {d[j]}")
+
+
+if __name__ == "__main__":
+    c = Solution()
+    sol = c.twoSum([5,5,11], 10)
+    print(sol)
